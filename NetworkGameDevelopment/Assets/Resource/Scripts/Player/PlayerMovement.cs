6b308ca614c957eb21b19aec27ebfa73e8a61cb6
Summer <gasawase@mail.uc.edu>
2024-10-10 10:10:19 -0400

    Module 3
    - Added ClientNetworkTransform script
    - Added simple player count Networked variable, synced, and tested
    - Added basic ownership to player movement, still need to polish actual movement, including adding rotation control
    - Added start buttons along with managers for host/server

--- NetworkGameDevelopment/Assets/Resource/Scripts/Player/PlayerMovement.cs
+++ NetworkGameDevelopment/Assets/Resource/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,14 @@
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Resource.Scripts.Player
 {
 
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : NetworkBehaviour
     {
         private void Update()
         {
+            if (!IsOwner) return; // if you are not the owner of this object, end this function
             Vector3 moveDirection = new Vector3(0, 0, 0);
             if (Input.GetKey(KeyCode.W)) moveDirection.z = +1f;
             if (Input.GetKey(KeyCode.S)) moveDirection.z = -1f;
